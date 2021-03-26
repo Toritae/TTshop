60f750dd8b931a7c847a7e878332597e49791e52
Toritae <60310842+Toritae@users.noreply.github.com>
2021-03-26 20:45:45 +0900

    Add files via upload

---
+++ TTshop/src/main/java/com/example/TTshop/TTshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.TTshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TTshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TTshopApplication.class, args);
+	}
+
+}
---
+++ TTshop/src/test/java/com/example/TTshop/TTshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TTshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TTshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
